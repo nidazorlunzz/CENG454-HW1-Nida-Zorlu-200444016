e0da0bb036f6303c87c7cae1961eccb8a2067480
nidazorlunzz <s200444016@stu.thk.edu.tr>
2026-03-22 22:07:14 +0300

    add last version and fox error

--- Assets/FlightController.cs
+++ Assets/FlightController.cs
@@ -2,22 +2,63 @@ using UnityEngine;
 
 public class FlightController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float pitchSpeed = 60f;   // Up / Down
+    [SerializeField] private float yawSpeed = 60f;     // Left / Right
+    [SerializeField] private float rollSpeed = 80f;    // Q / E
+    [SerializeField] private float thrustSpeed = 15f;  // Space
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        rb.freezeRotation = true;
+
+    
+        rb.useGravity = false;
     }
 
-    // Update is called once per frame
-    [SerializeField] float pitchSpeed = 150f;
-    [SerializeField] float thrustSpeed = 50f;
-    [SerializeField] float liftSpeed = 50f;
-    
     void Update()
     {
-        float pitchInput = Input.GetAxis("Vertical");
-        transform.Rotate(-pitchInput * pitchSpeed * Time.deltaTime, 0, 0);
-        transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime, Space.Self);
-       
+        HandleRotation();
+        HandleThrust();
+    }
+
+    private void HandleRotation()
+    {
+        float pitch = 0f;
+        float yaw = 0f;
+        float roll = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            pitch = -pitchSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            pitch = pitchSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            yaw = -yawSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            yaw = yawSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Q))
+            roll = rollSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            roll = -rollSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.right * pitch);
+        transform.Rotate(Vector3.up * yaw);
+        transform.Rotate(Vector3.forward * roll);
+    }
+
+    private void HandleThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime);
+        }
     }
 }
