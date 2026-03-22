284b2a3db884669736cfd4e053d7118b0c30b766
nidazorlunzz <s200444016@stu.thk.edu.tr>
2026-03-22 20:45:19 +0300

    add flight feature

--- Assets/FlightController.cs
+++ Assets/FlightController.cs
@@ -9,8 +9,15 @@ public class FlightController : MonoBehaviour
     }
 
     // Update is called once per frame
+    [SerializeField] float pitchSpeed = 150f;
+    [SerializeField] float thrustSpeed = 50f;
+    [SerializeField] float liftSpeed = 50f;
+    
     void Update()
     {
-        
+        float pitchInput = Input.GetAxis("Vertical");
+        transform.Rotate(-pitchInput * pitchSpeed * Time.deltaTime, 0, 0);
+        transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime, Space.Self);
+       
     }
 }
